00733e21047a42f6c130def62dc3aa93c8cee639
thedarkzeno <multiolx@gmail.com>
2021-03-28 14:51:28 -0300

    change

--- download_model.py
+++ download_model.py
@@ -3,5 +3,5 @@ from transformers import BertForMaskedLM, BertTokenizer
 model = BertForMaskedLM.from_pretrained("neuralmind/bert-base-portuguese-cased")
 tokenizer = BertTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
 
-model.save_pretrained("./model")
-tokenizer.save_pretrained("./model")
+model.save_pretrained("/home/models/neuralmind")
+tokenizer.save_pretrained("/home/models/neuralmind")
--- generate_training_data.py
+++ generate_training_data.py
@@ -363,9 +363,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--train_corpus', type=Path, required=True)
     parser.add_argument("--output_dir", type=Path, required=True)
-    parser.add_argument("--bert_model", type=str, required=True,
-                        choices=["bert-base-uncased", "bert-large-uncased", "bert-base-cased",
-                                 "bert-base-multilingual-uncased", "bert-base-chinese", "bert-base-multilingual-cased", "./model"])
+    parser.add_argument("--bert_model", type=str, required=True)
     parser.add_argument("--do_lower_case", action="store_true")
     parser.add_argument("--do_whole_word_mask", action="store_true",
                         help="Whether to use whole word masking rather than per-WordPiece masking.")
